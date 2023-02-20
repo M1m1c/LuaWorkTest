e42eb3bb62544cce2680ef4e5cabf69bce4a84d4
M1m1c <M1m1c@users.noreply.github.com>
2023-02-20 15:05:01 +0100

    started reworking gravity function

--- main.lua
+++ main.lua
@@ -1,8 +1,13 @@
 Vec2 = require("vec2")
 Player = require("player")
+Gravity= 100.0
+AccumulatedGravityAccel=0.0
+
+windowSize= Vec2.new(800.0,600.0)
 
 function love.load()
-    Player.Position= Vec2.new(0,100)
+    Player.Position= Vec2.new(100,0)
+    Player.Size= Vec2.new(10,10)
 end
 
 function love.update(dt)
@@ -11,17 +16,22 @@ end
 
 --TODO need to take in a table of entites
 function ApplyGravity(dt)
-    local gravityStep= 100 * dt 
+
+    AccumulatedGravityAccel= AccumulatedGravityAccel + (dt*3.5)
+    local gravityStep= Gravity * AccumulatedGravityAccel * dt 
     local nextStep = Player.Position.y + gravityStep
    
-    if nextStep < 400 then
+    if nextStep < windowSize.y-Player.Size.y then
         Player.Position.y= nextStep
+        
     else 
-        Player.Position.y = 400
+        Player.Position.y = windowSize.y-Player.Size.y
+        AccumulatedGravityAccel=0
     end
 end
 
 function love.draw()
-love.graphics.rectangle("fill",Player.Position.x,Player.Position.y,10,10)
+love.graphics.rectangle("fill",Player.Position.x,Player.Position.y, Player.Size.x, Player.Size.y)
 love.graphics.print("player position Y:" .. Player.Position.y, 0, 10)
+love.graphics.print("AccumulatedGravityAccel:" .. AccumulatedGravityAccel, 0, 20)
 end
