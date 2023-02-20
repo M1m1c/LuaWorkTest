fdcc1ffc8dc89c649366231890fe33d904cbcfa9
M1m1c <M1m1c@users.noreply.github.com>
2023-02-20 14:43:23 +0100

    added gravity function

--- main.lua
+++ main.lua
@@ -1,13 +1,27 @@
+Vec2 = require("vec2")
+Player = require("player")
 
 function love.load()
-    Vec2 = require("vec2")
-    Position= Vec2.new(0,100)
+    Player.Position= Vec2.new(0,100)
 end
 
 function love.update(dt)
-Position.x = Position.x +(dt*100)
+    ApplyGravity(dt)
+end
+
+--TODO need to take in a table of entites
+function ApplyGravity(dt)
+    local gravityStep= 100 * dt 
+    local nextStep = Player.Position.y + gravityStep
+   
+    if nextStep < 400 then
+        Player.Position.y= nextStep
+    else 
+        Player.Position.y = 400
+    end
 end
 
 function love.draw()
-love.graphics.rectangle("fill",Position.x,Position.y,100,100)
+love.graphics.rectangle("fill",Player.Position.x,Player.Position.y,10,10)
+love.graphics.print("player position Y:" .. Player.Position.y, 0, 10)
 end
