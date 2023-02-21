f0043df5bd90f385bf797c54e338d24a77f7b5af
M1m1c <M1m1c@users.noreply.github.com>
2023-02-21 14:42:40 +0100

    fixed bug with all inputs resulting in jump

--- inputComp.lua
+++ inputComp.lua
@@ -1,3 +1,5 @@
-local inputAtom={Current=false,Old=false}
-local inputComp = { Jump = inputAtom, Right = inputAtom, Left = inputAtom }
+local inputAtom1={Current=false,Old=false}
+local inputAtom2={Current=false,Old=false}
+local inputAtom3={Current=false,Old=false}
+local inputComp = { Jump = inputAtom1, Right = inputAtom2, Left = inputAtom3 }
 return inputComp
